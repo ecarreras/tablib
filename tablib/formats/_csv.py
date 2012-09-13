8302395f5095f16bd3a648be3042bac4d575fe07
Eduard Carreras <ecarreras@gisce.net>
2012-09-13 13:09:59 +0000

    Usig dataset delimiter attribute

--- tablib/formats/_csv.py
+++ tablib/formats/_csv.py
@@ -12,16 +12,24 @@ extensions = ('csv',)
 
 DEFAULT_ENCODING = 'utf-8'
 
+def get_delimiter(dataset):
+    try:
+        delimiter = dataset.delimiter
+    except AttributeError:
+        delimiter = ','
+    return delimiter
 
 
 def export_set(dataset):
     """Returns CSV representation of Dataset."""
     stream = StringIO()
 
+    delimiter = get_delimiter(dataset)
+
     if is_py3:
-        _csv = csv.writer(stream)
+        _csv = csv.writer(stream, delimiter=delimiter)
     else:
-        _csv = csv.writer(stream, encoding=DEFAULT_ENCODING)
+        _csv = csv.writer(stream, encoding=DEFAULT_ENCODING, delimiter=delimiter)
 
     for row in dataset._package(dicts=False):
         _csv.writerow(row)
@@ -33,11 +41,12 @@ def import_set(dset, in_stream, headers=True):
     """Returns dataset from CSV stream."""
 
     dset.wipe()
+    delimiter = get_delimiter(dset)
 
     if is_py3:
-        rows = csv.reader(in_stream.splitlines())
+        rows = csv.reader(in_stream.splitlines(), delimiter=delimiter)
     else:
-        rows = csv.reader(in_stream.splitlines(), encoding=DEFAULT_ENCODING)
+        rows = csv.reader(in_stream.splitlines(), encoding=DEFAULT_ENCODING, delimiter=delimiter)
     for i, row in enumerate(rows):
 
         if (i == 0) and (headers):
@@ -49,7 +58,7 @@ def import_set(dset, in_stream, headers=True):
 def detect(stream):
     """Returns True if given stream is valid CSV."""
     try:
-        csv.Sniffer().sniff(stream)
+        csv.Sniffer().sniff(stream, delimiter=delimiter)
         return True
     except (csv.Error, TypeError):
         return False
